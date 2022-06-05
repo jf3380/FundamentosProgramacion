00887c6590faea0d28bf0ddad1be236ed40bc45b
Furanshisuko <jf3380@gmail.com>
2022-06-04 20:08:42 -0400

    added while

---
+++ clases/ciclos/while.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+#define MAX 5
+
+int main(){
+        int contador = 0, i=0;
+	/*
+        estructura while
+        while (condicion){ bloue de codigo } 
+        */
+        while(contador<MAX){
+                contador = contador + 1
+		contador++;
+		printf("imprimiendo %d, \n", contador);
+	}
+
+	// bloue de codigo
+
+        return 0;
+}
